aa2c088860ac1a1fde6f00e6d55d1311c4fe0af2
Alex Ovdiichuk <alex.yokai00@gmail.com>
2024-07-27 15:37:49 +0300

    navbar added

---
+++ components/Avatar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+
+export const Avatar = () => {
+  return (
+    <div>
+      <Image
+        className="rounded-full"
+        height={30}
+        width={30}
+        alt="Avatar"
+        src="/images/placeholder.png"
+      />
+    </div>
+  );
+};
